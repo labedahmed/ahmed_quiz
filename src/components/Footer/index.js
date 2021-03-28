a0a60ef3402a279b551fa7a7d09f2d4738e1db99
ahmed <ea_labed@esi.dz>
2021-03-28 11:02:55 +0100

    message

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Labed Ahmed - 2021</p>
+                <p>Les icônes Wolverine, Iron-man, Spider-man, Batman sont prises sur iconFinder.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
